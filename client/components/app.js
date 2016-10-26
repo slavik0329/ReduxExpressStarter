afc7a27cf0dfc8aaa5e9e51a5893bf9106dcb048
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-10-26 13:34:41 -0500

    Update app.js

--- client/components/app.js
+++ client/components/app.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+simport React, { Component } from 'react';
 
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
-      <div>Redux Node starter</div>
+      <div>Redux Express Starter</div>
     );
   }
 }
+
+export default App;
